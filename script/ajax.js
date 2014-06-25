47cf56031d381858c09ff81396137f3d2755b3e6
Wim <wim@solydxk>
2014-06-25 23:27:25 +0200

    More pages added

---
+++ script/ajax.js
@@ -0,0 +1,29 @@
+var req = new XMLHttpRequest();
+
+function load(page){
+	console.log("Hi");
+	
+	req.open("GET","content/" + page + ".html",true);
+	req.send();
+}
+
+req.onreadystatechange = function(){
+if (req.readyState === 4 && req.status === 200){
+		//document.getElementById("body").innerHTML=req.responseText;
+		str = req.responseText;
+		
+		type();
+	}
+};
+
+var str, i = 0, text;
+
+function type() {
+	text = str.slice(0, ++i);
+	if (text === str) return;
+			
+	document.getElementById('body').innerHTML = str;
+	console.log("Type: " + str);
+	
+	setTimeout(type(), 100);
+}
